5085f3775efdde970d599b180b3d256952293bac
izumuru <izumuru1redstorm@gmail.com>
2023-05-25 15:18:51 +0700

    first commit

---
+++ migrations/20230525075201-create-user.js
@@ -0,0 +1,78 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.ENUM,
+        value: [
+            'admin',
+            'operator',
+            'customer'
+        ]
+      },
+      thumbnail: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      fcm_token: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      no_hp: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      alamat: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      gender: {
+        allowNull: true,
+        type: Sequelize.ENUM,
+        value: [
+            'LK',
+            'PR'
+        ]
+      },
+      isaktif: {
+        defaultValue: true,
+        type: Sequelize.BOOLEAN
+      },
+      ktp: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    user_id: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    type: DataTypes.ENUM,
+    thumbnail: DataTypes.TEXT,
+    username: DataTypes.STRING,
+    fcm_token: DataTypes.STRING,
+    no_hp: DataTypes.STRING,
+    alamat: DataTypes.TEXT,
+    gender: DataTypes.ENUM,
+    isaktif: DataTypes.BOOLEAN,
+    ktp: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
